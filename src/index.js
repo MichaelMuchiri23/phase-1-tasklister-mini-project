10c04abde48c87c262a67ae20831d9855d8a198d
Michael Muchiri <muchirimichael2222@gmail.com>
2023-10-05 00:28:40 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+    let form=document.querySelector('form')
+    let input=document.getElementById('new-task-description')
+    form.addEventListener('submit',(e)=>{
+    createFunction(form.querySelector('#new-task-description').value)
+    e.preventDefault()
+    form.reset()
+  })
 });
+
+function createFunction(todo){
+  let p=document.createElement('p')
+  let bttn=document.createElement('button')
+  p.textContent=`${todo}`
+  bttn.textContent='X'
+  document.querySelector('#list').appendChild(p)
+  p.appendChild(bttn)
+  bttn.addEventListener('click',deleteButton)
+}
+
+function deleteButton(e){
+  e.target.parentNode.remove()
+}
